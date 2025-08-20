01526dad3471d373d4d9c7fe1570662fdc52c530
renzo <renzo.chua@yahoo.com.sg>
2025-08-21 00:09:51 +0800

    refractor if-else loop to a switch

--- src/main/java/Boss.java
+++ src/main/java/Boss.java
@@ -9,30 +9,41 @@ public class Boss {
 
         ArrayList<Task> tasks = new ArrayList<>();
         Scanner scanner = new Scanner(System.in);
-        String input = scanner.nextLine();
-        while (!input.equals("bye")) {
-            if (input.equals("list")) {
-                for (int i = 0; i < tasks.size(); i++) {
-                    System.out.println((i + 1) + ": " + tasks.get(i));
+        while (true) {
+            String input = scanner.nextLine();
+            String cmdType = input.split(" ")[0];
+            switch (cmdType) {
+                case "bye": {
+                    System.out.println("Bye. Hope to see you again soon!");
+                    return;
                 }
-            } else if (input.startsWith("mark ")) {
-                int index = Integer.parseInt(input.substring(5)) - 1;
-                Task currentTask = tasks.get(index);
-                currentTask.setDone();
-                System.out.println("Nice! I've marked this task as done:");
-                System.out.println(currentTask);
-            } else if (input.startsWith("unmark ")) {
-                int index = Integer.parseInt(input.substring(7)) - 1;
-                Task currentTask = tasks.get(index);
-                currentTask.setNotDone();
-                System.out.println("OK, I've marked this task as not done yet:");
-                System.out.println(currentTask);
-            } else {
-                tasks.add(new Task(input));
-                System.out.println("added: " + input);
+                case "list": {
+                    for (int i = 0; i < tasks.size(); i++) {
+                        System.out.println((i + 1) + ": " + tasks.get(i));
+                    }
+                    break;
+                }
+                case "mark": {
+                    int index = Integer.parseInt(input.substring(5)) - 1; // remove "mark "
+                    Task currentTask = tasks.get(index);
+                    currentTask.setDone();
+                    System.out.println("Nice! I've marked this task as done:");
+                    System.out.println(currentTask);
+                    break;
+                }
+                case "unmark": {
+                    int index = Integer.parseInt(input.substring(7)) - 1; // remove "unmark "
+                    Task currentTask = tasks.get(index);
+                    currentTask.setNotDone();
+                    System.out.println("OK, I've marked this task as not done yet:");
+                    System.out.println(currentTask);
+                    break;
+                }
+                default: {
+                    tasks.add(new Task(input));
+                    System.out.println("added: " + input);
+                }
             }
-            input = scanner.nextLine();
         }
-        System.out.println("Bye. Hope to see you again soon!");
     }
 }
