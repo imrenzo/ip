70524f386c01a1d1c63d718408b084003f3db730
renzo <renzo.chua@yahoo.com.sg>
2025-08-20 23:11:22 +0800

    added array to store input

--- src/main/java/Boss.java
+++ src/main/java/Boss.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Boss {
@@ -6,13 +7,17 @@ public class Boss {
         System.out.println("Hello! I'm " + name);
         System.out.println("What can I do for you?");
 
+        ArrayList<String> lst = new ArrayList<>();
         Scanner scanner = new Scanner(System.in);
         String input = scanner.nextLine();
-        System.out.println(input);
-
         while (!input.equals("bye")) {
+            if (input.equals("list")) {
+                System.out.println(lst);
+            } else {
+                lst.add(input);
+                System.out.println("added: " + input);
+            }
             input = scanner.nextLine();
-            System.out.println(input);
         }
         System.out.println("Bye. Hope to see you again soon!");
     }
