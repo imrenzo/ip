b668e893462cc6116bbb6f3192c3d7209ab4acc4
renzo <renzo.chua@yahoo.com.sg>
2025-08-20 23:40:22 +0800

    added mark and unmark cmds

--- src/main/java/Boss.java
+++ src/main/java/Boss.java
@@ -7,16 +7,28 @@ public class Boss {
         System.out.println("Hello! I'm " + name);
         System.out.println("What can I do for you?");
 
-        ArrayList<String> lst = new ArrayList<>();
+        ArrayList<Task> tasks = new ArrayList<>();
         Scanner scanner = new Scanner(System.in);
         String input = scanner.nextLine();
         while (!input.equals("bye")) {
             if (input.equals("list")) {
-                for (int i = 0; i < lst.size(); i++) {
-                    System.out.println(i + 1 + ": " + lst.get(i));
+                for (int i = 0; i < tasks.size(); i++) {
+                    System.out.println((i + 1) + ": " + tasks.get(i));
                 }
+            } else if (input.startsWith("mark ")) {
+                int index = Integer.parseInt(input.substring(5)) - 1;
+                Task currentTask = tasks.get(index);
+                currentTask.setDone();
+                System.out.println("Nice! I've marked this task as done:");
+                System.out.println(currentTask);
+            } else if (input.startsWith("unmark ")) {
+                int index = Integer.parseInt(input.substring(7)) - 1;
+                Task currentTask = tasks.get(index);
+                currentTask.setNotDone();
+                System.out.println("OK, I've marked this task as not done yet:");
+                System.out.println(currentTask);
             } else {
-                lst.add(input);
+                tasks.add(new Task(input));
                 System.out.println("added: " + input);
             }
             input = scanner.nextLine();
