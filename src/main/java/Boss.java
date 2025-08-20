20daa0f9c310347fe1dc820b6216d544deb26358
renzo <renzo.chua@yahoo.com.sg>
2025-08-20 22:58:00 +0800

    bot echos cmd from user

--- src/main/java/Boss.java
+++ src/main/java/Boss.java
@@ -1,8 +1,19 @@
+import java.util.Scanner;
+
 public class Boss {
     public static void main(String[] args) {
         String name = "Boss";
         System.out.println("Hello! I'm " + name);
         System.out.println("What can I do for you?");
+
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine();
+        System.out.println(input);
+
+        while (!input.equals("bye")) {
+            input = scanner.nextLine();
+            System.out.println(input);
+        }
         System.out.println("Bye. Hope to see you again soon!");
     }
 }
