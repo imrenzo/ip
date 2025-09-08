8c42dca062a3e7b932ecbd233a3cac9cbfb2b329
renzo <renzo.chua@yahoo.com.sg>
2025-09-08 19:57:38 +0800

    Refactor Task's children to create Task instead
    Task.parseTask handles parsing for individual task commands.
    
    This makes logic very verbose.
    
    Let's move each command case to each children so that the children is responsible for creating task given the task information.
    
    This improves code clarity and adheres to the Single Responsibility Principle.

--- src/main/java/task/Events.java
+++ src/main/java/task/Events.java
@@ -1,5 +1,7 @@
 package task;
 
+import bossexceptions.BossException;
+
 /**
  * Events: tasks that start at a specific date/time and ends at a specific date/time.
  */
@@ -20,5 +22,31 @@ public class Events extends Task {
     @Override
     public String toString() {
         return "[" + commandCode + "]" + super.toString() + "(from: " + this.fromDate + " to: " + this.toDate + ")";
+    }
+
+    /**
+     * Creates an Events Task
+     * @param taskInfo Description of task
+     * @return Events Task
+     */
+    public static Events createTask(String taskInfo) throws BossException {
+        String[] splittedString = splitString(taskInfo);
+        String description = splittedString[0];
+        String fromDate = splittedString[1].trim();
+        String toDate = splittedString[2].trim();
+        return new Events(description, fromDate, toDate);
+    }
+
+    private static String[] splitString(String taskInfo) throws BossException {
+        String[] s = taskInfo.split("/from ", 2);
+        if (s.length < 2 || s[0].isBlank() || s[1].isBlank()) {
+            throw new BossException("Invalid format for event task.");
+        }
+        String description = s[0];
+        String[] dates = s[1].split("/to", 2);
+        if (dates.length < 2 || dates[0].isBlank() || dates[1].isBlank()) {
+            throw new BossException("Invalid format for start and end date/timings.");
+        }
+        return new String[]{description, dates[0].trim(), dates[1].trim()};
     }
 }
